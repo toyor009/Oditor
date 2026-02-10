9e36c887c86474ca7c7498823ab31fffab922151
Toyosi Olayiwola <toyorboy012@gmail.com>
2026-02-11 00:02:34 +0100

    project setup

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <UApp :toaster="{ position: 'top-right' }">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
+
+<script setup lang="ts"></script>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-dvh flex items-center">
+    <NuxtImg src="/images/logo-1.png" class="w-22 mx-auto" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
