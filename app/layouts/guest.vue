ee91972be6e3a061f89deb55fb8edd3e18d402fd
Toyosi Olayiwola <toyorboy012@gmail.com>
2026-02-16 21:36:11 +0100

    modify guest layout

--- app/layouts/guest.vue
+++ app/layouts/guest.vue
@@ -1,56 +1,65 @@
 <template>
-  <div class="h-dvh flex flex-col flex-1 p-4">
-    <!-- TODO: Build out the actual guest layout -->
-    <slot />
+  <div class="h-dvh overflow-auto">
+    <div class="h-full min-w-5xl overflow-x-scroll grid grid-cols-2">
+      <div class="w-100 mt-25 mx-auto">
+        <slot />
+      </div>
+
+      <div
+        class="bg-grey-25 p-4 border-l border-[#f2f2f2] flex items-center xl:items-end"
+      >
+        <NuxtImg src="/images/image-1.png" class="w-full xl:w-166 mx-auto" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { withoutTrailingSlash } from "ufo";
-import { useHead, useRoute, useSeoMeta } from "#imports";
+import { withoutTrailingSlash } from 'ufo';
+import { useHead, useRoute, useSeoMeta } from '#imports';
 
-import { capitalize } from "~/utils/misc";
+import { capitalize } from '~/utils/misc';
 
 const route = useRoute();
 
 const pageTitle = computed(
-  () => (route.meta.pageTitle as string | undefined) ?? "",
+  () => (route.meta.pageTitle as string | undefined) ?? '',
 );
 const seoTitle = computed(() => route.meta.seoTitle as string | undefined);
 const seoDescription = computed(
-  () => (route.meta.seoDescription as string | undefined) ?? "",
+  () => (route.meta.seoDescription as string | undefined) ?? '',
 );
 const seoImage = computed(() => route.meta.seoImage as string | undefined);
 
 useHead({
   meta: [
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-    { key: "theme-color", name: "theme-color", content: "#007AFF" },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: '#007AFF' },
   ],
   link: [
-    { rel: "icon", type: "image/png", href: "/images/favicon.png" },
+    { rel: 'icon', type: 'image/png', href: '/images/favicon.png' },
     {
-      rel: "canonical",
+      rel: 'canonical',
       href: `https://app.theoditor.com/${withoutTrailingSlash(route.path)}`,
     },
   ],
   htmlAttrs: {
-    class: "h-full bg-white",
-    lang: "en",
+    class: 'h-full bg-white',
+    lang: 'en',
   },
   bodyAttrs: {
-    class: "h-full",
+    class: 'h-full',
   },
 });
 
 useSeoMeta({
   title: computed(() => `Oditor  | ${capitalize(String(pageTitle.value))}`),
   ogTitle: computed(
-    () => `Oditor  | ${seoTitle.value || capitalize(pageTitle.value) || ""}`,
+    () => `Oditor  | ${seoTitle.value || capitalize(pageTitle.value) || ''}`,
   ),
   description: computed(() => seoDescription.value),
   ogDescription: computed(() => seoDescription.value),
-  ogImage: computed(() => seoImage.value || "/images/logo-1.png"),
-  twitterCard: "summary_large_image",
+  ogImage: computed(() => seoImage.value || '/images/logo-1.png'),
+  twitterCard: 'summary_large_image',
 });
 </script>
--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,34 +1,25 @@
 <template>
   <div class="flex flex-col flex-1">
     <NuxtImg src="/images/logo-1.png" class="w-22" />
-
-    <div class="flex flex-col flex-1 items-center justify-center">
-      <UButton
-        class="px-10"
-        leading-icon="i-lucide-log-in"
-        label="Log in"
-        @click="login"
-      />
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useAuthStore } from "~/stores/useAuthStore";
+import { useAuthStore } from '~/stores/useAuthStore';
 
-defineOptions({ name: "SalesPage" });
+defineOptions({ name: 'SalesPage' });
 
 definePageMeta({
-  layout: "guest",
-  pageTitle: "Login",
-  middleware: ["guest"],
+  layout: 'guest',
+  pageTitle: 'Login',
+  middleware: ['guest'],
 });
 
 const auth = useAuthStore();
 
 async function login() {
-  const { data } = await auth.login({ email: "test@mail.com", password: "" });
+  const { data } = await auth.login({ email: 'test@mail.com', password: '' });
 
-  if (data.success) navigateTo("/", { replace: true });
+  if (data.success) navigateTo('/', { replace: true });
 }
 </script>
