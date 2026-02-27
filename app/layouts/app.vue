86587db1ec5a252f60d312d42d5e18b1204e270b
Toyosi Olayiwola <toyorboy012@gmail.com>
2026-02-27 15:48:16 +0100

    modify in-app layout structure

--- app/layouts/app.vue
+++ app/layouts/app.vue
@@ -3,12 +3,12 @@
     <!-- Sidebar -->
     <Sidebar :page-id="pageId || ''" />
 
-    <div class="main-content">
+    <div class="main-content max-w-500 mx-auto">
       <!-- Navbar -->
       <!-- <Navbar /> -->
 
-      <main class="main-scrollable max-w-360 mx-auto">
-        <div class="flex flex-col flex-1 py-4 px-8">
+      <main class="main-scrollable">
+        <div class="min-w-236 overflow-x-scroll flex flex-col flex-1 py-4 px-8">
           <!-- Page Content -->
           <slot />
         </div>
@@ -18,60 +18,60 @@
 </template>
 
 <script setup lang="ts">
-import { withoutTrailingSlash } from "ufo";
-import { useHead, useRoute, useSeoMeta } from "#imports";
+import { withoutTrailingSlash } from 'ufo';
+import { useHead, useRoute, useSeoMeta } from '#imports';
 
-import Sidebar from "~/components/ui/Sidebar.vue";
+import Sidebar from '~/components/ui/Sidebar.vue';
 
-import { capitalize } from "~/utils/misc";
+import { capitalize } from '~/utils/misc';
 
-import type { AppRouteId } from "~/types/generic";
+import type { AppRouteId } from '~/types/generic';
 
 const route = useRoute();
 
 const pageId = computed(
-  () => (route.meta.pageId as AppRouteId | undefined) ?? ("" as AppRouteId),
+  () => (route.meta.pageId as AppRouteId | undefined) ?? ('' as AppRouteId),
 );
 
 const pageTitle = computed(
-  () => (route.meta.pageTitle as string | undefined) ?? "",
+  () => (route.meta.pageTitle as string | undefined) ?? '',
 );
 const seoTitle = computed(() => route.meta.seoTitle as string | undefined);
 const seoDescription = computed(
-  () => (route.meta.seoDescription as string | undefined) ?? "",
+  () => (route.meta.seoDescription as string | undefined) ?? '',
 );
 const seoImage = computed(() => route.meta.seoImage as string | undefined);
 
 useHead({
   meta: [
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-    { key: "theme-color", name: "theme-color", content: "#007AFF" },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: '#007AFF' },
   ],
   link: [
-    { rel: "icon", type: "image/png", href: "/images/favicon.png" },
+    { rel: 'icon', type: 'image/png', href: '/images/favicon.png' },
     {
-      rel: "canonical",
+      rel: 'canonical',
       href: `https://app.theoditor.com/${withoutTrailingSlash(route.path)}`,
     },
   ],
   htmlAttrs: {
-    class: "h-full bg-white",
-    lang: "en",
+    class: 'h-full bg-white',
+    lang: 'en',
   },
   bodyAttrs: {
-    class: "h-full",
+    class: 'h-full',
   },
 });
 
 useSeoMeta({
   title: computed(() => `Oditor  | ${capitalize(String(pageTitle.value))}`),
   ogTitle: computed(
-    () => `Oditor  | ${seoTitle.value || capitalize(pageTitle.value) || ""}`,
+    () => `Oditor  | ${seoTitle.value || capitalize(pageTitle.value) || ''}`,
   ),
   description: computed(() => seoDescription.value),
   ogDescription: computed(() => seoDescription.value),
-  ogImage: computed(() => seoImage.value || "/images/logo-1.png"),
-  twitterCard: "summary_large_image",
+  ogImage: computed(() => seoImage.value || '/images/logo-1.png'),
+  twitterCard: 'summary_large_image',
 });
 </script>
 
@@ -99,7 +99,6 @@ useSeoMeta({
   display: flex;
   flex-direction: column;
   flex: 1;
-  overflow-y: auto;
-  overflow-x: hidden;
+  overflow: auto;
 }
 </style>
